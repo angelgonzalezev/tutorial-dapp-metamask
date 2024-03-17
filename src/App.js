c1c87f15ecaff61d03813454fe2224f9b22fc58b
Angel Gonzalez <aangel.gg@hotmail.com>
2024-03-17 12:42:42 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Box w={"100vw"} h={"100vh"} m={8}></Box>;
 }
 
 export default App;
